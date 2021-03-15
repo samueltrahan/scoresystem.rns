97bc6027ab6e30f8cfec8e89c79eee3399b90629
Samuel Trahan <samtrahan91@gmail.com>
2021-03-15 14:24:53 -0500

    navbar styling fix

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,30 +7,24 @@ const NavBar = ({ user, handleLogout }) => {
   let nav = user ? 
     <>
       <nav className="nav-bar">
+        <div className="nav-logo">
+          <a href="/"><img className="logo" src="/images/golf-ball-logo.png" alt=""></img></a>
+        </div>
         <div className="nav-wrapper">
-          <ul className="right">
-            <li>
-              <Link to={`/user/${user.id}`}>Welcome, {user.name}</Link>
-            </li>
-            <li>
-              <Link to=" " onClick={handleLogout}>Log Out</Link>
-            </li>
-          </ul>
+              <Link className="nav-links" id="user" to={`/user/${user.id}`}>Welcome, {user.name}</Link>
+              <Link className="nav-links" id="logout" to=" " onClick={handleLogout}>Log Out</Link>
         </div>
       </nav>
     </>
     :
     <>
       <nav className="nav-bar">
+        <div className="nav-logo">
+        <img className="logo" src="/images/golf-ball-logo.png" alt=""></img>
+        </div>
         <div className="nav-wrapper">
-          <ul className="right">
-            <li>
-              <Link to="/login">Log In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-          </ul>
+              <Link className="nav-links" id="login" to="/login">Log In</Link>
+              <Link className="nav-links" id="signup" to="/signup">Sign Up</Link>
         </div>
       </nav>
     </>
