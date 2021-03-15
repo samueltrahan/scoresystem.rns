a48ca06616609ac572e2ed1fee5e9912ccd99a38
Samuel Trahan <samtrahan91@gmail.com>
2021-03-15 11:57:55 -0500

    scaffolding and set up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+export default function App() {
+    return (
+        <div>
+           <NavBar />
+        </div>
+    )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+    return (
+        <div className="nav-bar">
+            <div className="heading">
+            <h2>Samuel's React App</h2>
+            </div>
+        </div>
+    )
+}
