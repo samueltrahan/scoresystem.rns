552c4e04ab070fbb06665386f47ca0ed965a2469
Samuel Trahan <samtrahan91@gmail.com>
2021-03-18 10:17:27 -0500

    new round button added to score card page

--- src/pages/ScoreCardPage/ScarecardPage.js
+++ src/pages/ScoreCardPage/ScarecardPage.js
@@ -6,6 +6,9 @@ import Scorecard from '../../components/Scorecard/Scorecard';
 export default function ScarecardPage() {
     return (
         <div>
+            <div>
+                <button className="new-round-btn">New Round</button>
+            </div>
             {scoreCardDB.map(scorecard => 
              <Scorecard scorecard={scorecard} />    
             )}
