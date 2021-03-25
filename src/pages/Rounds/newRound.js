70219a04f81305d790188cc469158d0b1efe4655
Samuel Trahan <samtrahan91@gmail.com>
2021-03-25 11:37:48 -0500

    some styling to the new rounds page

--- src/pages/Rounds/newRound.js
+++ src/pages/Rounds/newRound.js
@@ -6,10 +6,17 @@ export default function NewRound() {
   const [roundId, setRoundId] = useState(uuidv4());
   const [nickName, setNickName] = useState("");
   const [date, setDate] = useState();
-  const [course, setCourse] = useState(1);
+  const [course, setCourse] = useState("The Farm D'Alie Golf Course");
   return (
     <div>
-      <form>New Round Form</form>
+      <form className="new-round-form">
+          <h3>New Round</h3>
+          <div className="new-round-input">
+        <input className="new-round-input" placeholder="Name Your Round" default={course}></input>
+        <input className="new-round-input" type="date"></input>
+          </div>
+        <button className="new-round-btn" type="submit">Start Round</button>
+      </form>
     </div>
   );
 }
